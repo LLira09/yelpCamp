e8ae080a1b5beb5d1f02fa8a471c32ce7ac56274
Jake Lira <llira09@gmail.com>
2021-09-22 10:43:49 -0500

    added isLoggedin, isAuthor and validateCampground middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,29 +1,20 @@
 const express = require('express')
 const router = express.Router({ mergeParams: true })
-const ExpressError = require('../utils/ExpressError')
+const { validateReview } = require('../middleware')
 const catchAsync = require('../utils/catchAsync')
 const Campground = require('../models/campground')
 const Review = require('../models/review')
-const { reviewSchema } = require('../schemas.js')
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body)
-  if (error) {
-    const msg = error.details.map(el => el.message).join(',')
-    throw new ExpressError(msg, 400)
-  } else {
-    next()
-  }
-}
 
 router.post(
   '/',
+  validateReview,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     const review = new Review(req.body.review)
     campground.reviews.push(review)
     await review.save()
     await campground.save()
+    req.flash('success', 'Created new review!')
     res.redirect(`/campgrounds/${campground._id}`)
   })
 )
